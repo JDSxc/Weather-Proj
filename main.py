450b22906f5af7ab0641c297d82fe71054c23764
NamNguyenUTSA <namnguyenucf@gmail.com>
2025-07-27 17:44:25 -0500

    Removed old imports. Replaced "Time" placeholder in index.html with current Day/Date. Reformatted `2025-07-27` style API dates to `Mon 07/27` style (old format dates still accessible through `raw_dates` if needed). Added current location label to HTML template. Force case on city, state, and country.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
 from flask import Flask, request, render_template
 
 from weather import get_lat_long, get_current_weather, get_forecast
 from secrets_helper import get_api_key
+from datetime import datetime
 
 API_KEY = get_api_key()
 
@@ -12,11 +12,13 @@ app.jinja_env.globals.update(zip=zip) # Allow use of zip() in our HTML templates
 @app.route("/")
 def show_weather():
     # Default to San Antonio, if no GET args are provided
-    city = request.args.get("city", "San Antonio")
-    state = request.args.get("state", "Tx")
-    country = request.args.get("country", "United States")
+    city = request.args.get("city", "San Antonio").title() # Normalize capitalization
+    state = request.args.get("state", "TX").upper()
+    country = request.args.get("country", "United States").title() 
 
     lat, lon = get_lat_long(city, state, country, API_KEY)
+
+    current_date = datetime.now().strftime("%A - %B %d, %Y") # i.e., format as Monday - July 27, 2025
 
     # This checks if lat or lon is returned based on city name input (Assuming state and country are always valid).
     if lat is None or lon is None:
@@ -27,7 +29,8 @@ def show_weather():
                 country=country,
                 error=f"City not found: {city}",
                 current=None,
-                forecast=None
+                forecast=None,
+                current_date=current_date
         )
 
     current = get_current_weather(lat, lon)
@@ -41,7 +44,8 @@ def show_weather():
       country=country,
       error=None,
       current=current,
-      forecast=forecast
+      forecast=forecast,
+      current_date=current_date
     )
 
 # Run via the Flask development server if running main.py directly
